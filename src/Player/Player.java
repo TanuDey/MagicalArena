06f3373a9438c229f38aad6b3abd47c850a2ca49
TanuDey <tanu036@outlook.com>
2024-06-30 20:38:37 +0530

    Created battle method

--- src/Player/Player.java
+++ src/Player/Player.java
@@ -33,12 +33,36 @@ class Player {
     public String getName() {
         return name;
     }
+    public boolean isAlive() {
+        return health > 0;
+    }
 
-   
+    public int rollDice(Scanner scanner, String playerName, String rollType) {
+        int roll;
+        do {
+            System.out.print(playerName + ", enter your " + rollType + " roll (1-6): ");
+            roll = scanner.nextInt();
+        } while (roll < 1 || roll > 6);
+        return roll;
+    }
+
+    public void attack(Player defender, Scanner scanner) {
+        int attackRoll = rollDice(scanner, this.name, "attack");
+        int defenseRoll = defender.rollDice(scanner, defender.getName(), "defense");
+        
+        int damageDealt = this.attack * attackRoll;
+        int damageDefended = defender.getStrength() * defenseRoll;
+        int damageToDefender = Math.max(damageDealt - damageDefended, 0);
+        
+        defender.setHealth(defender.getHealth() - damageToDefender);
+        
+        System.out.println(this.name + " attacks " + defender.getName() + " and rolls a " + attackRoll);
+        System.out.println(defender.getName() + " defends and rolls a " + defenseRoll);
+        System.out.println(this.name + " deals " + damageToDefender + " damage to " + defender.getName());
+        System.out.println(defender.getName() + " health is now " + defender.getHealth());
+    }
 
 }
-
-
 
 class MagicalArena {
     private Player playerA;
@@ -51,7 +75,28 @@ class MagicalArena {
         this.scanner = scanner;
     }
 
- 
+    public void startBattle() {
+        System.out.println("Battle starts between " + playerA.getName() + " and " + playerB.getName());
+        while (playerA.isAlive() && playerB.isAlive()) {
+            if (playerA.getHealth() < playerB.getHealth()) {
+                playerA.attack(playerB, scanner);
+                if (playerB.isAlive()) {
+                    playerB.attack(playerA, scanner);
+                }
+            } else {
+                playerB.attack(playerA, scanner);
+                if (playerA.isAlive()) {
+                    playerA.attack(playerB, scanner);
+                }
+            }
+        }
+
+        if (playerA.isAlive()) {
+            System.out.println(playerA.getName() + " wins the battle!");
+        } else {
+            System.out.println(playerB.getName() + " wins the battle!");
+        }
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -82,6 +127,7 @@ class MagicalArena {
         Player playerB = new Player(nameB, healthB, strengthB, attackB);
 
         MagicalArena arena = new MagicalArena(playerA, playerB, scanner);
+        arena.startBattle();
        
         scanner.close();
     }
